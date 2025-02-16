456751800c2e84b16a65b6fef8b054f1f3789a02
awl1991 <awl1991@gmail.com>
2025-02-16 09:07:08 -0600

    remove suspense

--- app/search/children-wrapper.tsx
+++ app/search/children-wrapper.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { Fragment, Suspense } from 'react';
+import { Fragment } from 'react';
 
 // Ensure children are re-rendered when the search query changes
 export default function ChildrenWrapper({ children }: { children: React.ReactNode }) {
   const searchParams = useSearchParams();
-  return <Fragment key={searchParams.get('q')}><Suspense>{children}</Suspense></Fragment>;
+  return <Fragment key={searchParams.get('q')}>{children}</Fragment>;
 }
