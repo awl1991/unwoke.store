81760224f610dd982b35d85ecb81429b93981837
awl1991 <awl1991@gmail.com>
2025-02-16 09:03:52 -0600

    try to fix search

--- app/search/children-wrapper.tsx
+++ app/search/children-wrapper.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { Fragment } from 'react';
+import { Fragment, Suspense } from 'react';
 
 // Ensure children are re-rendered when the search query changes
 export default function ChildrenWrapper({ children }: { children: React.ReactNode }) {
   const searchParams = useSearchParams();
-  return <Fragment key={searchParams.get('q')}>{children}</Fragment>;
+  return <Fragment key={searchParams.get('q')}><Suspense>{children}</Suspense></Fragment>;
 }
